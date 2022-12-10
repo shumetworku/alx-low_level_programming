822219d01ad844bf89932263b6092653794c4ae1
shumetworku <shumwor@gmail.com>
2022-12-10 12:03:36 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char l;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+
+	putchar('\n');
+
+	return (0);
+}
