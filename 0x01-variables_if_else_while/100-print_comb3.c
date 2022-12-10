7bd76e3e9206644114d7a39ec405803936dda557
shumetworku <shumwor@gmail.com>
2022-12-10 12:46:33 -0800

    different combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)/*print tens digit*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)/*print ones digit*/
+			if (!((ones == tens) || (tens > ones)))/*eliminates repititio*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/*add comma and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	}
+}
