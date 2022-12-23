4addfcf977f537dad58dbeab2398ea36485f7a54
shumetworku <shumwor@gmail.com>
2022-12-23 04:48:47 -0800

    concatinate strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,15 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * void prototypes()
- * int prototypes()
- * char prototypes()
- */
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char s2);
+int _strcmp(char *s1, char *s2);
 void reverse_aray(int *a, int n);
 char *string_toupper(char *a);
 char *cap_string(char *a);
