127458284d3ecff0efba2863edfed7743b461b06
shumetworku <shumwor@gmail.com>
2022-12-23 04:38:15 -0800

    concatinate string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,14 +8,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len, i;
+	int i;
+	int j;
 
-	for (dest_len = 0; dest[dst_len] != '\0';)
-		dest_len++;
-	for (i =0; i < n && src[i] != '\0';)
+	i = 0;
+	while (dest[i] != ;\0')
+	{
 		i++;
-	dest[dest_len + i] = src[i];
-
-	dest[dest_len + i] = '\0';
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
